895d929722f51d562897197ce31acff86b1e9d2f
Sharena <sdbaron08@gmail.com>
2018-03-16 13:50:16 -0400

    Initial commit

---
+++ src/main/java/com/example/demo/BullhornStep2Application.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BullhornStep2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BullhornStep2Application.class, args);
+	}
+}
